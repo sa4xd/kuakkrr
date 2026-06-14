929f253c2899b541aa38f18faac87b0e6905adae
sa4xd <xxuhjx@mkij8jjo08.ip-ddns.com>
2026-06-14 13:35:32 +0800

    Update keepalive.py

--- keepalive.py
+++ keepalive.py
@@ -3,9 +3,6 @@ import datetime
 
 def main():
     print("Keepalive job started.")
-    while True:
-        now = datetime.datetime.utcnow().isoformat()
-        print(f"[{now}] still alive...")
 
 
 if __name__ == "__main__":
